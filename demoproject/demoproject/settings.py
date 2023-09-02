1e002e1814668d07bb809cf0104c08c06c81d4ce
almazuulu <almazuulu@gmail.com>
2023-09-03 00:30:48 +0600

    маленький недочет исправлен

--- demoproject/demoproject/settings.py
+++ demoproject/demoproject/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -118,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'demoproject/static')
